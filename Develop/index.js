737897c619c89921f1ff84ba132adc21e14be8ff
JimmyPedone <>
2023-05-29 21:49:46 -0400

    first draft of readme code generation

---
+++ Develop/index.js
@@ -0,0 +1,86 @@
+//The following variables are what is required for the README generator.
+  //const inquirer is installed to display questions
+  //const fs is in-place which is needed anytime I'm switching files
+  //const generateMarkdown pulls code from generateMarkdown.js to index.js
+
+const inquirer = require('inquirer');
+  // console.log(inquirer);
+const fs = require('fs');
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+//The following code is an array of questions that should be answered by the user to generate an already formatted README.md that is applicable to the project. 
+
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name of your project?"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Write a description for your project.  Use the following questions to guide your thinking: What was your motivation? Why did you build this project? What problem does it solve? What did you learn? What makes your project stand out?"
+  },
+  {
+    type: "input",
+    name: "installationInstructions",
+    message: "How should the user install the application?"
+  },
+  {
+    type: "input",
+    name: "usageInformation",
+    message: "What is the name of your project?"
+  },
+  {
+    type: "input",
+    name: "contributionGuidelines",
+    message: "How should engineers contribute to your project? Create steps for creating pull requests, links to external documentation, mailing lists, or a code of conduct, and community/behavioral expectations."
+  },
+  {
+    type: "input",
+    name: "testInstructions",
+    message: "Give users explicit instructions on how to run all necessary tests. Explain libraries used for testing your software and supply all necessary commands."
+  },
+  {
+    type: "checkbox",
+    name: "licence",
+    message: "Select an appropriate license for your project:",
+    choices: ["MIT", "Boost1.0", "Apache2.0", "None"],
+  },
+  {
+    type: "input",
+    name: "creator",
+    message: "Add your GitHub username here:"
+  },
+  {
+    type: "input",
+    name: "name",
+    message: "Add your full name here:"
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Add your email address here:"
+  },
+
+  
+];
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+  fs.writeFile('README.md', data, (err) =>
+  err ? console.error(err) : console.log('Success!'));
+}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions).then((responses) => {
+    console.log("Created README.md File...");
+    writeToFile("./Develop/README.md", generateMarkdown({ ...responses }));
+  });
+}
+// Function call to initialize app
+init();
+
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,63 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (license !== "None") {
+    return `![Github license](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`;
+  }
+  return "";
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  if (license !== "None") {
+    return `\n* [License](#licence)\n`;
+  }
+  return "";
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
+  if(license !== "None") {
+    return `## License
+
+      Licensed under the ${license} license.`;
+  }
+  return "";
+  
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title}
+by ${data.name}
+${renderLicenseBadge(data.licence)}
+
+## Table of Contents
+  *[Description](#description)
+  *[Installation](#installationInstructions)
+  *[Usage](#usage-information)
+  *[Contributing](#contributionGuidelines)
+  *[Test Instructions](#testInstructions)
+  *[Questions](#questions)
+
+${renderLicenseLink(data.license)}
+## Description
+${data.description}
+## Installation
+${data.installationInstructions}
+## Usage
+${data.usageInformation}
+## Contributing
+${data.contributionGuidelines}
+## Tests
+${data.testInstructions}
+## Questions
+* Name - ${data.name}
+* Email - ${data.email}
+* Email - [${data.creator}](https://github.com/${data.creator}/)
+
+${renderLicenseSection(data.license)}
+`;}
+
+module.exports = generateMarkdown;
